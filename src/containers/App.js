b26721dc15c4caf8ffcf212c2ce446fd052f0688
TahinDani <tahin.daniel@gmail.com>
2018-07-11 19:30:09 +0200

    requesting robots with redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,48 +4,40 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 // What piece of state I need to listen to and send down as props
 const mapStateToProps = (state) => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error,
     }
 }
 
 // What props I should listen to that are actions 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots()),
     }
 }
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-        }
-    }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())   // return in one line
-        .then(users => {                    //
-            this.setState({ robots: users}) // return in multiple lines, could convert to one line too
-        });                                 //
+        this.props.onRequestRobots();
     }
 
     render() {
-        const { robots } = this.state;
-        const { searchField, onSearchChange } = this.props;
-        const filteredRobots = this.state.robots.filter(robot => {
+        const { searchField, onSearchChange, robots, isPending } = this.props;
+        const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
-        if (robots.length === 0){
-            return <h1>Loading</h1>
-        } else {
-            return (
+        return isPending ?
+             <h1>Loading</h1> :
+             (
                 <div className='tc'>
                     <h1 className='f1'>Robofriends</h1>
                     <SearchBox searchChange={onSearchChange}/>
@@ -55,8 +47,8 @@ class App extends Component {
                 </div>
             );
         }
-    }
 }
+
 
 // We have created an action that gets dispatched into the reducer
 // that - based on that action - takes the state and changes it in the store.
--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk'; // Waits and sees if any actions return a function instead of an object
 
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
 // Middleware
 const logger = createLogger();
 
-const store = createStore(searchRobots, applyMiddleware(logger));
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 // Provider component is passing down the store to all the components
 // down the component tree from the App
